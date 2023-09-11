12e9dd6e90a34abd9bf78d9e2cf5adf241b4daa1
Aleena712 <77184400+Aleena712@users.noreply.github.com>
2023-09-11 12:53:04 +0530

    Add files via upload

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Title({ Title }) {
+    return (
+        <div className='section-title'>
+            <h4>{Title}</h4>
+            <div />
+
+        </div>
+    )
+}
